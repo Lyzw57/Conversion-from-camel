d3059e85db3603c98b0733646af474b9d2f26070
Lyzw57 <askuczynski57@gmail.com>
2019-03-19 20:57:53 +0100

    fixed errors

--- convert_camel.py
+++ convert_camel.py
@@ -1,15 +1,13 @@
 def convert_from_camel(phrase: str):
     python_format = ""
 
-    for i in range(len(phrase)):
-        if phrase[i].isupper():
-            python_format += phrase[i].lower()
-        elif phrase[i+1].isupper():
+    for i in range(len(phrase)-1):
+        if phrase[i+1].isupper():
             python_format += phrase[i] + "_"
         else:
             python_format += phrase[i]
 
-    return python_format
+    return python_format.lower() + phrase[-1]
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
